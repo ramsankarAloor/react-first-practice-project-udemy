85eb4e06e3ef6f12d0f7eba34545bd0dabefc346
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-07 12:22:08 +0530

    using states and clearing input field by feeding back the state value to input value attribute

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username"></input>
+        <input
+          type="text"
+          id="username"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id="age"></input>
+        <input
+          type="number"
+          id="age"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
