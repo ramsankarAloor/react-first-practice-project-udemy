1326dddd9ca50261751de106681a5933538b206b
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-07 13:30:29 +0530

    usersList basic structure

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,12 @@
+import Card from '../UI/Card';
+import classes from './UsersList.module.css'
+
+const UsersList=props=>{
+    return <Card className={classes.users}>
+        <ul>
+
+        </ul>
+    </Card>
+}
+
+export default UsersList;
