4ff89f628ceab48534dc16a4dd048cbef93dcad4
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-07 20:25:31 +0530

    conditional rendering of hide class

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -3,7 +3,7 @@ import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
   return (
-    <Card className={classes.users}>
+    <Card className={`${classes.users} ${props.users.length===0 ? classes.hide : ''}`}>
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
